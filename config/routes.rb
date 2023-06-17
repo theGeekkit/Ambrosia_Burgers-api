2a6e314fe1a596e290288244a909964a678bf871
theGeekkit <sonofgondolin16@gmail.com>
2023-06-17 11:14:24 -0500

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :authentications
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, param: :_username
